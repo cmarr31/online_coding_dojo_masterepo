213d79d16596129c9a2b874ffcf891c19a3efea7
cmarr31 <chris@sitefuelusa.com>
2017-09-08 13:21:13 -0500

    With help from Devon in the code review, found the solution to 'multiplication table' assignment.

--- python_week_1/thurs_sept_7/multiplication_table.py
+++ python_week_1/thurs_sept_7/multiplication_table.py
@@ -1,8 +1,15 @@
 def multiplication_table():
 	x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-	for number in x:
-		count = 1
-		while count <= len(x):
-			print number * count
-			count +=1
+	row = "x "
+	for item in x:
+		row += "{} ".format(item)
+	print row
+
+	for i in range(1, 13):
+		row = [y*i for y in range(1, len(x)+1)]
+		sub = ""
+		for item in row:
+			sub += "{} ".format(item)
+		print sub
+
 multiplication_table()
