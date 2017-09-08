597bc06c027716b9f0d56f89a3f89d67a8789c31
cmarr31 <chris@sitefuelusa.com>
2017-09-08 16:39:14 -0500

    Draw stars assignment.

--- python_week_1/fri_sept_8/stars.py
+++ python_week_1/fri_sept_8/stars.py
@@ -8,7 +8,7 @@
 # 			count +=1
 # 		print empty_str
 
-# x = [4, 6, 1, 3, 5, 7, 25]
+y = [4, 6, 1, 3, 5, 7, 25]
 # draw_stars(x)
 
 # ------------------------------
@@ -32,4 +32,5 @@ def draw_stars(the_list):
 
 x = [4, "Tom", 1, "Michael", 5, 7, "Jimmy Smith"]
 
+draw_stars(y)
 draw_stars(x)
