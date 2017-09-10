6744fa94c270b9b00a4cc802fe0396dbcffab731
cmarr31 <chris@sitefuelusa.com>
2017-09-10 16:11:46 -0500

    Making Tuples assignment.

--- python_week_1/fri_sept_8/dictionary_in_tuple_out.py
+++ python_week_1/fri_sept_8/dictionary_in_tuple_out.py
@@ -1,1 +1,14 @@
-def dictionary_in_tuple_out()
+def dictionary_in_tuple_out(dictionary):
+	items = dictionary.items()
+	print items
+
+
+
+my_dict = {
+	"Speros": "(555) 555-5555",  
+	"Michael": "(999) 999-9999",  
+	"Jay": "(777) 777-7777"
+}
+
+
+dictionary_in_tuple_out(my_dict)
