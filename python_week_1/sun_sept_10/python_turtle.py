0c36da95dd72fc655f52a411607dbaff68b20d9f
cmarr31 <chris@sitefuelusa.com>
2017-09-10 17:48:00 -0500

    Have no idea what to do with 'turtle' assignment.

--- python_week_1/sun_sept_10/python_turtle.py
+++ python_week_1/sun_sept_10/python_turtle.py
@@ -1,19 +1,14 @@
 # try this either as a .py file or in the python shell
 import turtle
 # the distance we want the pointer to travel each time
-# window = turtle.Screen()
-# ella = turtle.Turtle()
-# dist = 100
+DIST = 100
 for x in range(0,6):
-	window = turtle.Screen()
-	ella = turtle.Turtle()
-	dist = 100
     print "x", x
     for y in range(1,5):
         print "y", y
         # turn the pointer 90 degrees to the right
-        ella.right(90)
+        turtle.right(90)
         # advance according to set distance
-        ella.forward(dist)
+        turtle.forward(DIST)
     # add to set distance
-    dist += 20
+    DIST += 20
