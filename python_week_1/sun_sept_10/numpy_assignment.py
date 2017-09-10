b37ca9b069912640c361d1225f3484458f242c31
cmarr31 <chris@sitefuelusa.com>
2017-09-10 18:15:56 -0500

    .

---
+++ python_week_1/sun_sept_10/numpy_assignment.py
@@ -0,0 +1,12 @@
+import numpy as np
+print a = np.arange(15).reshape(3, 5)
+print a
+print a.shape
+print a.ndim
+print a.dtype.name
+print a.itemsize
+print a.size
+print type(a)
+print b = np.array([6, 7, 8])
+print b
+print type(b)
