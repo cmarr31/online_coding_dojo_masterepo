cbc3bb2416174a45a5f432562948713d63f42284
cmarr31 <chris@sitefuelusa.com>
2017-09-17 14:37:59 -0500

    Dojo Survey assignment.

--- python_week_2/fri_sept_15/dojo_survey/server.py
+++ python_week_2/fri_sept_15/dojo_survey/server.py
@@ -1,28 +1,30 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)   
+from flask import Flask, render_template, redirect, request, session, flash
 
+app = Flask(__name__)
+app.secret_key = "dojo"
 
-@app.route('/')                                             
-def user_info():
-  return render_template('index.html') 
+@app.route('/')
+def index():
+	return render_template('index.html')
 
-
-@app.route('/result', methods=['POST'])
-def display_info():
-	print "User's Information"
-	name = request.form['users_name']
-	location = request.form['location']
-	language = request.form['language']
-	comment = request.form['comment']
-	return redirect('/')
-
-
-# @app.route('/dojos/new')
-# def dojos():
-#   return render_template('dojos.html')
-
-
-
-
-
+@app.route('/result', methods=["POST"])
+def result():
+	session['name'] = request.form['name']
+	session['location'] = request.form['location']
+	session['language'] = request.form['language']
+	session['comment'] = request.form['comment']
+	error = False
+	if len(session['name']) < 1:
+	    flash("Name is a required field")
+	    error = True
+	if len(session['comment']) < 1:
+	    flash("Comment is a required field")
+	    error = True
+	if len(session['comment']) > 120:
+	    flash("Comment cannot be longer than 120 characters!")
+	    error = True
+	if error == True:
+	    return redirect('/')
+	else:
+	    return render_template('result.html')
 app.run(debug=True)
